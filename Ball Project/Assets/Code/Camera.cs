5b05da2ae4c9dc14b4bcdc282389f454ddd59f76
larnbda <147326831+larnbda@users.noreply.github.com>
2025-11-24 22:22:11 +0900

    created_temp_map

--- Ball Project/Assets/Code/Camera.cs
+++ Ball Project/Assets/Code/Camera.cs
@@ -7,18 +7,18 @@ public class Camera : MonoBehaviour
     public GameObject targetCamera;
     public GameObject targetObject;
 
-    private Vector3 offset; //카메라와 타켓 사이 거리
+    public Vector3 offset = new Vector3(0, 1, -2); 
+    //카메라와 타켓 사이 거리 = 타켓카메라의 트랜스폼 위치 빼기 타겟오브젝트 트랜스폼 위치
     
     void Start()
     {
-        offset = targetCamera.transform.position - targetObject.transform.position;
-        //거리 = 타켓카메라의 트랜스폼 위치 빼기 타겟오브젝트 트랜스폼 위치
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        targetCamera.transform.position = offset + targetObject.transform.position;
+        targetCamera.transform.position = targetObject.transform.position + offset;
         //타겟 카메라 위치 설정(거리+타겟오브젝트위치)
     }
 }
